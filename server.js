f452c220637eba3aa536d7563bac3f8bf73a9137
therealgemjosh <therealgemjosh@gmail.com>
2018-10-03 13:52:15 +0100

    version 0.0.1

--- server.js
+++ server.js
@@ -5,17 +5,12 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const route = require('./route/controller');
 const DB = require('./config/database').mongoURI;
-
 // init express;
 const app = express();
 app.use(bodyparse.json());
 // serve static assets if in production;
-if(process.env.NODE_ENV == 'production'){
-  // set static folder;
+if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'));
-  app.get('*', (req,res)=> {
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-  })
 }
 // init route func;
 route(app);
