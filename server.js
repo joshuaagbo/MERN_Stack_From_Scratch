ebe162608827862c9ff59c6e7e3d3f87463b81ee
therealgemjosh <therealgemjosh@gmail.com>
2019-01-10 17:44:34 +0000

    clean up

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(bodyparse.json());
 // serve static assets if in production;
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'));
+  app.use('*', (req,res)=> {
+      res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
+  });
 }
 // init route func;
 route(app);
